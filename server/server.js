f0ecd4060779933c41ee948b738cc8855224fef3
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-09-12 15:45:43 +0800

    return 200 status in express

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/:message', (req, res)=>{
-    res.status(500).send(req.params.message);
+    res.status(200).send(req.params.message);
 })
 
 app.listen(8001, () => console.log('Server is running on port 8001'))
