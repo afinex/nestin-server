5368def0e3bdf31778447821c92bd8e8ed664a59
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-09-19 11:55:25 +0800

    using dotenv package as .env like laravel

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import {readdirSync} from 'fs';
+require("dotenv").config();
 
 const app = express();
 readdirSync('./routes').map((e_route)=>{
     app.use('/api', require(`./routes/${e_route}`));
 });
 
-app.listen(8001, () => console.log('Server is running on port 8001'))
+const port = process.env.PORT || 8001;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
