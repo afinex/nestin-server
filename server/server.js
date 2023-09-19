109eb75eea6d18cd3448d3a9cc78c317473c35a2
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-09-19 12:02:32 +0800

    setup cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {readdirSync} from 'fs';
+import cors from 'cors';
 const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(morgan("dev"));
 
 // route middleware
