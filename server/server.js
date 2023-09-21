2a79129f5fb9ebee402cb11243bc54d82944083a
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-09-21 19:36:22 +0800

    initialize mongoose, cors , dotenv , morgan , express

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import {readdirSync} from 'fs';
 import cors from 'cors';
+import mongoose from 'mongoose';
 const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
 
 //middleware
+mongoose.connect(process.env.DATABASE)
+.then(()=> console.log(`Database connected`))
+.catch((e)=>console.log(`Error : ${e}`));
+
 app.use(cors());
 app.use(morgan("dev"));
 
