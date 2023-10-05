a734aa7ec6b1ca98e01099b77b5f58662825e30c
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-10-05 16:26:53 +0800

    added Register API

---
+++ server/controllers/authController.js
@@ -0,0 +1,8 @@
+export const page = (req, res) => {
+    res.status(200).send(`Here is your message: ${req.params.message}`);
+}
+
+export const register = async(req, res) =>{
+
+    console.log(req.body);
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {page} from '../controllers/showMessageController';
+import {page, register} from '../controllers/authController';
 
 const router = express.Router();
 
 router.get('/:message', page)
+
+router.post('/register', register)
 
 module.exports = router;
