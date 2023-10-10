2b6e192e7abb5d46da55a1cb05bc07de46826159
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-10-10 14:02:39 +0800

    add User model Schema

---
+++ server/models/users.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: 'Email is required',
+  },
+  password: {
+    type: String,
+    min: 6,
+    max: 64,
+    required: 'Password is required',
+  },
+  stripe_account_id: '',
+  stripe_seller: {},
+  stripeSession: {},
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isModified("password")) {
+      const hash = await bcrypt.hash(this.password, 12);
+      this.password = hash; 
+    }
+    next();
+  } catch (error) {
+    console.error('BCRYPT HASH ERR', error);
+    next(error);
+  }
+});
+
+export default mongoose.model("User", userSchema);
