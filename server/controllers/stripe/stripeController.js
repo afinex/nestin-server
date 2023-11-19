2b05787a7ca7a3b240c030e076253f9b31fff2c7
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-11-20 03:49:00 +0800

    updated  : create user's stripe account on button click install : stripe package

--- server/controllers/stripe/stripeController.js
+++ server/controllers/stripe/stripeController.js
@@ -1,11 +1,30 @@
+import User from "../../models/users";
+import Stripe from "stripe";
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 export const createConnectAccount = async (req, res) => {
   if (req.user) {
-    res.status(200).json({
-      success: true,
-      data: req.user,
-      message: 'User data received successfully'
-    });
+    const user = await User.findById(req.user._id).exec();
+
+    if(!user.stripe_account_id){
+      const account = await stripe.accounts.create({
+        type: 'standard',
+      });
+
+      user.stripe_account_id = account.id;
+      user.save();
+      console.log(`Registered Stripe Account`);
+
+    }
+    else{
+      console.log(`Stripe Account :`,user.stripe_account_id);
+    }
+
   } else {
-    res.status(401).json({ success: false, message: 'Unauthorized' });
+    res.status(401).json({
+      success: false,
+      message: 'Unauthorized' 
+    });
   }
 };
